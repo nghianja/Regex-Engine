af60709fd1c02eab2636b48f7b6ca0ff9a25f7a3
James Ng <james.ng@projapps.com>
2020-09-20 12:11:07 +0800

    Complete Stage #1

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -1,1 +1,15 @@
 # write your code here
+
+def compare(regex, input):
+    if regex == '':
+        return "True"
+    if regex == '.':
+        return "True"
+    if regex == input:
+        return "True"
+    return "False"
+
+
+if __name__ == "__main__":
+    parameters = input().split('|')
+    print(compare(parameters[0], parameters[1]))
