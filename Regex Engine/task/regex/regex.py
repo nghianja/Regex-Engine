e7a4e25a42e9da0eeb2f273647aa9ae6311186ba
James Ng <james.ng@projapps.com>
2020-09-20 16:46:51 +0800

    Complete Stage #5

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -1,11 +1,7 @@
 # write your code here
 
 def compare(regex, input):
-    if regex == '':
-        return True
-    if regex == '.':
-        return True
-    if regex == input:
+    if regex == '' or regex == '.' or regex == input:
         return True
     return False
 
@@ -17,6 +13,19 @@ def comparator(regex, input):
         return True
     if input == '':
         return False
+    if len(regex) > 1:
+        if (regex[1] == '?' or regex[1] == '*') and compare(regex[0], input[0]) is False:
+            return comparator(regex[2:], input)
+        if regex[1] == '?' and compare(regex[0], input[0]) is True:
+            return comparator(regex[2:], input[1:])
+        if regex[1] == '*' or regex[1] == '+':
+            i = 0
+            while compare(regex[0], input[i]) is True:
+                i += 1
+                if i >= len(input) or input[i] != input[i-1]:
+                    break
+            if i > 0:
+                return comparator(regex[2:], input[i:])
     if compare(regex[0], input[0]) is False:
         return False
     return comparator(regex[1:], input[1:])
